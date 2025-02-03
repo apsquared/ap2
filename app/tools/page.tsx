b1fce171a59004a8802a1c3dbfd46dfbac968557
Info APSquared <info@apsquared.co>
2025-02-03 17:17:54 -0500

    fix 404

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -26,7 +26,7 @@ export default function ToolsDisplay() {
     <div className="container mx-auto py-12">
       <h1 className="text-3xl font-bold mb-8 text-center">Our AI Agents</h1>
       <p className="text-lg mb-8 text-left">
-        The following are some sample AI Agents that we have built using our <a className="text-blue-500 hover:underline" href="https://www.apsquared.co/full-stack-ai-agents">full stack agent &quot;framework&quot;</a>.
+        The following are some sample AI Agents that we have built using our <a className="text-blue-500 hover:underline" href="https://www.apsquared.co/posts/full-stack-ai-agents">full stack agent &quot;framework&quot;</a>.
         Each agent can be run in the browser and contains links to the source code and a graph of the agent.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
